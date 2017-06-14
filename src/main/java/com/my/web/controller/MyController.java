fb22535183443acd9aa9bb7752ded0344d698e31
Shawn <azaaza987@gmail.com>
2017-06-14 20:49:06 +0800

    update controller class

--- src/main/java/com/my/web/controller/MyController.java
+++ src/main/java/com/my/web/controller/MyController.java
@@ -3,9 +3,7 @@
  */
 package com.my.web.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,67 +11,67 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.my.web.entity.User;
+import com.my.web.service.UserService;
 
 @Controller
 public class MyController {
-	private Map<String,User> users=new HashMap<String,User>();
 	
-	public MyController(){
-		users.put("Tom", new User("Tom","M","20","Tom@test.com"));
-		users.put("Jerry", new User("Jerry","F","21","Jerry@test.com"));
-		users.put("Jason", new User("Jason","M","25","Jason@test.com"));
-		users.put("Iva", new User("Iva","F","30","Iva@test.com"));
-		
-	}
+	@Autowired  
+	private UserService userService ;
 	
 	@RequestMapping("login")
-	public String hello(
-			String username,
-			String password
-			
-			){
-		if (username.equals("Admin") && password.equals("Admin")){
-			
+	public String hello(String username, String password
+
+	) {
+		if (username.equals("Admin") && password.equals("Admin")) {
+
 			return "sucess";
-			
-		}else
-		return "fail";
+
+		} else
+			return "fail";
 	}
-	@RequestMapping(value="list",method=RequestMethod.GET)
-	public String list(Model model){
-		model.addAttribute("user", users);
+
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public String list(Model model) {
+		model.addAttribute("user", userService.getUserList());
 		return "list";
 	}
-	@RequestMapping(value="add",method=RequestMethod.GET) 
-	public String add(Model model){
+
+	// this method used to direct to Add page
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public String add(Model model) {
 		model.addAttribute(new User());
 		return "add";
 	}
-	@RequestMapping(value="add",method=RequestMethod.POST)
-	public String add(User user){
-		users.put(user.getName(), user);
+
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public String add(User user) {
+		userService.addUser(user);
 		return "redirect:list";
 	}
-	@RequestMapping(value="/{name}", method=RequestMethod.GET)
-	public String show(@PathVariable String name,Model model){
-		model.addAttribute(users.get(name));
+
+	// This method used to direct to update page
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String show(@PathVariable String name, Model model) {
+		model.addAttribute(userService.showUser(name));
 		return "show";
 	}
-	
-	@RequestMapping(value="/{name}/update",method=RequestMethod.GET)
-	public String update(@PathVariable String name,Model model){
-		model.addAttribute(users.get(name));
+
+	@RequestMapping(value = "/{name}/update", method = RequestMethod.GET)
+	public String update(@PathVariable String name, Model model) {
+		model.addAttribute(userService.showUser(name));
 		return "update";
 	}
-	
-	@RequestMapping(value="/{name}/update",method=RequestMethod.POST)
-	public String update(@PathVariable String name,User user){
-		users.put(name, user);
+
+	@RequestMapping(value = "/{name}/update", method = RequestMethod.POST)
+	public String update(@PathVariable String name, User user) {
+		userService.update(name, user);
 		return "redirect:../list";
 	}
-	@RequestMapping(value="/{name}/delete",method=RequestMethod.GET)
-	public String delete(@PathVariable String name){
-		users.remove(name);
+
+	@RequestMapping(value = "/{name}/delete", method = RequestMethod.GET)
+	public String delete(@PathVariable String name) {
+		userService.delete(name);
 		return "redirect:../list";
 	}
 }
